754ddfb2252d496c275aa6bc79f2b023f842cffe
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2021-09-17 15:18:05 +0200

    First Level

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+ 
+    public Vector3 getPosition()
+    {
+        return transform.position;
+    }
+}
